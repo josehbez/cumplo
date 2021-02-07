554dfb18ee5168dc144a14a0fcab2a45096eff62
José Hernández <jose.hbez@gmail.com>
2021-02-06 20:34:26 -0600

    add urls for bm

---
+++ cumplo/static/js/bm.js
@@ -0,0 +1,188 @@
+(function ($) {
+/*
+var randomScalingFactor = function() {
+    return  Math.floor(Math.random() * Math.floor(50));
+};
+chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+
+var config = {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'DOLAR',
+            fill: false,
+            backgroundColor: chartColors.blue,
+            borderColor: chartColors.blue,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+        }, {
+            label: 'UDIS',
+            fill: false,
+            backgroundColor: chartColors.green,
+            borderColor: chartColors.green,
+            borderDash: [5, 5],
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+        }, {
+            label: 'TIIE 28',
+            backgroundColor: chartColors.red,
+            borderColor: chartColors.red,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+            fill: false,
+        },{
+            label: 'TIIE 91',
+            backgroundColor: chartColors.red,
+            borderColor: chartColors.red,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+            fill: false,
+        },{
+            label: 'TIIE 28',
+            backgroundColor: chartColors.red,
+            borderColor: chartColors.red,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+            fill: false,
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Histórico'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Día'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Valor'
+                }
+            }]
+        }
+    }
+};
+*/
+
+    function GetHistorical (){
+        var hc = document.getElementById('historical-chart');
+        if (hc !== null){
+            var ctx = hc.getContext('2d');
+            fetch('/api/historical').then( response => response.json()).then( data=>{                
+                if (data.success == true){
+                    var hcConfig = {
+                        type: 'line',
+                        data: {
+                            labels: data.payload.labels,
+                            datasets:data.payload.datasets
+                        },
+                        options: {
+                            responsive: true,
+                            title: {
+                                display: true,
+                                text: 'Histórico'
+                            },
+                            tooltips: {
+                                mode: 'index',
+                                intersect: false,
+                            },
+                            hover: {
+                                mode: 'nearest',
+                                intersect: true
+                            },
+                            scales: {
+                                xAxes: [{
+                                    display: true,
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: 'Día'
+                                    }
+                                }],
+                                yAxes: [{
+                                    display: true,
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: 'Valor'
+                                    }
+                                }]
+                            }
+                        }
+                    };
+                    new Chart(ctx, hcConfig);
+                }else{
+                    console.error("Error:", data.message)
+                }
+                console.log(data)
+            }).catch((error)=>{
+                console.error("Error:", error)
+            });
+            
+            
+        }
+    }
+
+    $(document).ready(function () {
+        GetHistorical();
+    });
+
+})(window.jQuery);
